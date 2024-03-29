89b532ca23d852fa133808d48c094b5635e44900
Dilimulati Diliyaer <dilyarswe@gmail.com>
2024-03-29 13:37:00 -0400

    more ml agent training

--- Assets/FlapAI/BaseGame/Scripts/TrainingPipes.cs
+++ Assets/FlapAI/BaseGame/Scripts/TrainingPipes.cs
@@ -19,7 +19,7 @@ public class TrainingPipes : MonoBehaviour
     private void Start() {
         Transform bottom = transform.Find("SPR_BottomPipe");
         Transform top = transform.Find("SPR_TopPipe");
-        float width = Academy.Instance.EnvironmentParameters.GetWithDefault("pipe_width", 1.6f);
+        float width = Academy.Instance.EnvironmentParameters.GetWithDefault("pipe_width", 8f);
         float added_pos = (width - 1.6f) / 2f;
         bottom.transform.position += Vector3.down * added_pos;
         top.transform.position += Vector3.up * added_pos;
